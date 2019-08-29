26bb6335aefd5cbf18b537c55d0c34694f082d47
Markham Meredith <markham.meredith002@gmail.com>
2019-08-29 19:47:25 +1200

    still a 403 error, called chmod on www and www2 folders incase apache couldnt access

---
+++ www/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN"><!DOCTYPE html>
+<html>
+  <head>
+    <title>Contact Database</title>
+    <meta charset=utf-8>
+  </head>
+  <body>
+    <header>
+      <h1>Contact Database</h1>
+      <nav>
+	<div id ="menu"><h2>Enter Details</h2></div>
+	<div id ="menu"><h2>Contact Information</h2></div>
+      </nav> 
+      
+      <form action="/action_page.php">
+	<fieldset>
+	  <legend>Enter Personal Information:</legend>
+	  First name:<br>
+	  <input type="text" name="firstname" value=""><br>
+	  Last name:<br>
+	  <input type="text" name="lastname" value=""><br>
+	  Phone Number:<br>
+	  <input type="text" name="phonenum" value=""><br>
+	  Email Address:<br>
+	  <input type="text" name="email" value=""><br><br>
+	  <input type="submit" value="Submit">
+	</fieldset>
+      </form>
+     
+
+  </body>
+</html>
