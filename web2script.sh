1cdae997d40c7ae526d41247c69fa4d0ebbac0cf
Markham Meredith <markham.meredith002@gmail.com>
2019-08-29 17:42:24 +1200

    fixed typo

--- web2script.sh
+++ web2script.sh
@@ -2,9 +2,9 @@ apt-get update
 apt-get install -y apache2 php libapache2-mod-php php-mysql
 
 
-cp /vagrant/assign2-website.conf /etc/apache2/sites-available/
+cp /vagrant/assign2-site.conf /etc/apache2/sites-available/
 # activate our website configuration ...
-a2ensite assign2-website
+a2ensite assign2-site
 # ... and disable the default website provided with Apache
 a2dissite 000-default
 # Reload the webserver configuration, to pick up our changes
