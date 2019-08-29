f372f384c36c907ea304103673dda8ac72cb3cbe
Markham Meredith <markham.meredith002@gmail.com>
2019-08-29 17:09:45 +1200

    adding database setup sql

---
+++ dbsetup.sql
@@ -0,0 +1,13 @@
+CREATE TABLE contactInfo (
+ firstname varchar(255),
+ lastname varhcar(255) NOT NULL,
+ phonenum varchar(20) NOT NULL,
+ email varchar(80)
+ PRIMARY KEY (phonenum)
+ );
+
+INSERT INTO contactInfo VALUES ('Markham', 'Meredith', '027776912423', 'markham@markham.com');
+INSERT INTO contactInfo VALUES ('Mark', 'Meredith', '027776915643', 'mar@mham.com');
+
+
+
