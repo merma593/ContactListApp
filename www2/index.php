58e35c0bfda6cfae80866dbb78979be6c79a0a45
Markham Meredith <markham.meredith002@gmail.com>
2019-09-01 14:55:57 +1200

    updated contact list website

--- www2/index.php
+++ www2/index.php
@@ -8,13 +8,13 @@
     <header>
       <h1>Contact Database</h1>
       <nav>
-	<div id ="menu"><a href="http://192.168.34.11"><h2>Enter Details</h2></a></div>
+	<div id ="menu"><a href="http://192.168.34.11"><h2>Add new Contact</h2></a></div>
 	<div id ="menu"><h2>Search Contacts</h2></a></div>
 </nav> 
 
 
 <table border="1">
-  <tr><th>Name</th><th>NumberPaper name</th></tr>
+  <tr><th>First Name</th><th>Last Name</th><th>Phone number</th><th>Email</th></tr>
 
   <?php
    
@@ -30,7 +30,7 @@
    $q = $pdo->query("SELECT * FROM contactInfo");
 
   while($row = $q->fetch()){
-  echo "<tr><td>".$row["firstname"]."</td><td>".$row["phonenum"]."</td></tr>\n";
+  echo "<tr><td>".$row["firstname"]."</td><td>".$row["lastname"]."</td><td>".$row[phonenum]."</td><td>".$row[email]."</td></tr>\n";
   }
 
   ?>
