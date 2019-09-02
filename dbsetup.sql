b6ca98e183a4550e04aa9fe4f795bf4f2f5064fe
merma593 <33414491+merma593@users.noreply.github.com>
2019-09-02 13:21:15 +1200

    Update dbsetup.sql

--- dbsetup.sql
+++ dbsetup.sql
@@ -6,5 +6,6 @@ CREATE TABLE contactInfo (
  PRIMARY KEY (phonenum)
  );
 
-
+INSERT INTO contactInfo VALUES ('Markham', 'Meredith', '027776912423', 'markham@markham.com');
+INSERT INTO contactInfo VALUES ('ollie', 'whiteman', '027776915643', 'ollie@whiteman.com');
 
